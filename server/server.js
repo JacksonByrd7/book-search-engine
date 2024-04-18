7c9e0cda4b0bcd75da4b62f05b542066422970f5
JacksonByrd7 <jackson.byrd7@gmail.com>
2024-04-18 15:36:44 -0400

    strucutred directory, wrote server.js

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const db = require('./ocnfig/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: ture}));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('oepn', () => {
+    app.listen(PORT, () => console.log(` Now listening on localhost:${PORT}`));
+});
